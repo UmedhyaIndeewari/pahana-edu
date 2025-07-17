748e4f0d01763cb75ab9ab4979aa4447e69cf50b
UmedhyaIndeewari <umedhyaindeewari@gmail.com>
2025-07-17 22:29:59 +0530

    chore:Login servlet added

--- src/main/java/com/icbt/controller/LoginServlet.java
+++ src/main/java/com/icbt/controller/LoginServlet.java
@@ -1,4 +1,44 @@
 package com.icbt.controller;
 
-public class LoginServlet {
+import com.icbt.model.User;
+import com.icbt.service.UserService;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class LoginServlet extends HttpServlet {
+    private UserService userService = new UserService();
+
+    public void init(){userService = new UserService();}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        User user = userService.login(username, password);
+
+        if(user != null){
+            httpSession session = request.getSession();
+            session.setAttribute("user", user);
+            RequestDispatcher rd = request.getRequestDispatcher("main-menu.jsp");
+            rd.forward(request, response);
+        } else {
+            request.setAttribute("error", "Invalid username or password.");
+            RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+            rd.forward(request, response);
+        }
+
+        }
+
+    }
+
+
+
+
+
+
 }
